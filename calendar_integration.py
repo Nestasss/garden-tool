bd5be55f95dc02d9619a07187ae6ddce73e7e87e
Nestasss <141137180+Nestasss@users.noreply.github.com>
2025-11-10 00:57:26 +0300

    Update calendar_integration.py

--- calendar_integration.py
+++ calendar_integration.py
@@ -1,9 +1,19 @@
 from googleapiclient.discovery import build
-from google.oauth2.credentials import Credentials
-from config import GOOGLE_CALENDAR_API_KEY, GOOGLE_CALENDAR_ID
+from google.oauth2.service_account import Credentials
+from config import GOOGLE_CALENDAR_ID
+import json
+import os
+
+# Загружаем ключ из GitHub Secrets
+SERVICE_ACCOUNT_KEY = os.getenv("GOOGLE_SERVICE_ACCOUNT_KEY")
+if SERVICE_ACCOUNT_KEY:
+    creds_info = json.loads(SERVICE_ACCOUNT_KEY)
+    creds = Credentials.from_service_account_info(creds_info, scopes=['https://www.googleapis.com/auth/calendar'])
+else:
+    raise ValueError("GOOGLE_SERVICE_ACCOUNT_KEY not set")
 
 def create_calendar_event(summary, location, start_time, end_time):
-    service = build("calendar", "v3", developerKey=GOOGLE_CALENDAR_API_KEY)
+    service = build('calendar', 'v3', credentials=creds)
 
     event = {
         'summary': summary,
